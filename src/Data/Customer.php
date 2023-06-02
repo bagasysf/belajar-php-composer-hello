702b2c239327276119972cd02723a06427e6a24a
bagasysf <bagasysf@gmail.com>
2023-06-02 09:17:12 +0700

    Added default value

--- src/Data/Customer.php
+++ src/Data/Customer.php
@@ -3,7 +3,7 @@ namespace Babang\Data;
 
 class Customer {
 
-  public function __construct(private string $name) {}
+  public function __construct(private string $name = "Tamu") {}
 
   public function sayHello(string $name) {
     echo "Hello $name, my name is $this->name" . PHP_EOL;
